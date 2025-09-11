42d3bb21a63f17bea3a6f59d73077b6c26649145
itsukiii <weizx17@outlook.com>
2025-09-11 17:18:07 +0800

    feat: Initial commit for all three projects (monorepo)

---
+++ vue3-demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello, World!</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 50px;
+  color: #42b983;
+}
+</style>
